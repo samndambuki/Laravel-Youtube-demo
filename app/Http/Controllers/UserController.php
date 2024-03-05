883feaf9b2ac2d6948640b1a4d2362f446d3825b
samndambuki <sam.ndambuki08@gmail.com>
2024-03-05 09:13:09 +0300

    run create_users table

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function register(Request $request){
+        $incomingFields = $request->validate([
+            'name' => ['required','min:3','max:10'],
+            'email' => ['required','email'],
+            'password' => ['required','min:8','max:200']
+        ]);
+        return 'Hello from our controller';
+    }
 }
